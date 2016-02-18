cb3103e0e7f8bc9bd825d0ab8793e09b4351e50e
Richard Attermeyer <richard.attermeyer@gmail.com>
2016-02-18 22:16:50 +0100

    additional multi module with dependencies

---
+++ another-multi-module/anothermodule1/src/main/de/rattermeyer/demo/module1/MyModule1.java
@@ -0,0 +1,7 @@
+package de.rattermeyer.demo.module1;
+
+public class MyModule1 {
+  public static void main(String[] args) {
+    System.out.println("Hello, world from module1");
+  }
+}
---
+++ another-multi-module/anothermodule2/src/main/de/rattermeyer/demo/module2/MyModule2.java
@@ -0,0 +1,7 @@
+package de.rattermeyer.demo.module1;
+
+public class MyModule2 {
+  public static void main(String[] args) {
+    System.out.println("Hello, world from module2");
+  }
+}
